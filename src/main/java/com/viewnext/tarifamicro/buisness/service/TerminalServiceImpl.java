5c589dea21c6d2461aa6e65ddefdd4c53a7f5dba
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-21 09:37:05 +0200

    Estética del código

--- src/main/java/com/viewnext/tarifamicro/buisness/service/TerminalServiceImpl.java
+++ src/main/java/com/viewnext/tarifamicro/buisness/service/TerminalServiceImpl.java
@@ -1,11 +1,15 @@
 package com.viewnext.tarifamicro.buisness.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.viewnext.tarifamicro.model.Terminal;
 import com.viewnext.tarifamicro.reader.TerminalReader;
@@ -19,22 +23,26 @@ import lombok.Data;
 @Service
 public class TerminalServiceImpl implements TerminalService {
 
-	Map<Integer, Terminal> terminalRep;
-	
+	private Map<Integer, Terminal> terminalRep;
+
+	@Autowired
+	@Qualifier("registrarTemplateTarifas")
+	private RestTemplate clienteRest;
+
 	public TerminalServiceImpl() {
 		terminalRep = new HashMap<>();
 	}
-	
+
 	@Override
 	public List<Terminal> adjuntarTarifa(List<Terminal> terminales) {
 		List<Terminal> terminalesTemp = TerminalReader.adjuntarTarifas(terminales);
-		
+
 		for (Terminal terminal : terminalesTemp) {
 			if (terminal.getPrecio() != null) {
 				terminalRep.put(terminal.getId(), terminal);
 			}
 		}
-		
+
 		return new ArrayList<>(terminalRep.values());
 	}
 

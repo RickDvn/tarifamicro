5e96ffca7c8eaf6fab68fdd633284afa8f5d87e0
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-21 10:05:52 +0200

    Añadidos comentarios

--- src/main/java/com/viewnext/tarifamicro/config/AppConfig.java
+++ src/main/java/com/viewnext/tarifamicro/config/AppConfig.java
@@ -4,9 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuracion para los RestTemplates
+ */
 @Configuration
 public class AppConfig {
 	
+	/**
+	 * Devuelve un nuevo RestTemplate para comunicar a los micros (solo necesario si no se usa Feign)
+	 * 
+	 * @return RestTemplate
+	 */
 	@Bean("registrarTemplateTarifas")
 	RestTemplate registerRestTemplate() {
 		return new RestTemplate();
--- src/main/java/com/viewnext/tarifamicro/feign/client/TerminalClienteRest.java
+++ src/main/java/com/viewnext/tarifamicro/feign/client/TerminalClienteRest.java
@@ -7,9 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.viewnext.tarifamicro.model.Terminal;
 
+/**
+ * Clase que contiene todas las llamadas al micro: batch-micro
+ */
 @FeignClient(name = "batch-micro")
 public interface TerminalClienteRest {
 
+	/**
+	 * Llama al endpoint /terminales/getCatalogo de batch-micro para obtener la lista de
+	 * terminales
+	 * 
+	 * @return List<Terminal>
+	 */
 	@GetMapping(value = "/terminales/getCatalogo")
 	public List<Terminal> getTerminales();
 	
